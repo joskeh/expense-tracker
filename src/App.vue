7586e268b753bd09f8b9568cfcea0796d8d8c32a
joskeh <joskeh@live.com>
2024-02-01 21:47:57 -0600

    added transaction lists

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import AddTransaction from './components/AddTransaction.vue';
+import TransactionList from './components/TransactionList.vue';
 import {ref, computed} from   'vue'
 
 
@@ -69,7 +70,9 @@ const generateUniqueId = () => {
     <div class="container">
         <Balance :total="total"></Balance>
         <IncomeExpenses :income="income" :expense="expense"></IncomeExpenses>
+        <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"></TransactionList>
+
         <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
-        {{ transactions }}
+        <!-- {{ transactions }} -->
     </div>
 </template>
